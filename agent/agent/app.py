103bf0575fa9b1602a1ed997a882e9a517ff311e
benvarian <88700858+benvarian@users.noreply.github.com>
2023-08-18 10:31:26 +0800

    first

--- agent/agent/app.py
+++ agent/agent/app.py
@@ -13,7 +13,6 @@ for step in range(5000):
     action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
     done = terminated or truncated
-
     if done:
         state = env.reset()
 
