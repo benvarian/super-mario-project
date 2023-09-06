04cd60e98f61e457b676c450640887e7d6aa5722
benvarian <88700858+benvarian@users.noreply.github.com>
2023-09-06 15:41:27 +0800

    first iteration of something working

--- agent/agent/app.py
+++ agent/agent/app.py
@@ -1,97 +1,84 @@
-# Install pytorch   
-#poetry add torch torchvision torchaudio
+# Install pytorch
+# poetry add torch torchvision torchaudio
 # install stable baselines for RL algorithm
-#poetry add stable-baselines3[extra]
+# poetry add stable-baselines3[extra]
 # pip3 install stable-baselines --use-deprecated=backtrack-on-build-failures
 # poetry add  stable-baselines3[extra]
 
 # https://stackoverflow.com/questions/73195438/openai-gyms-env-step-what-are-the-values
 
-# new 
+# new
 import gym_super_mario_bros
-import gym
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from gym.wrappers import frame_stack, GrayScaleObservation
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-from stable_baselines3.common.env_checker import check_env
 from matplotlib import pyplot as plt
 
-env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True,  render_mode='human')
+# rl model
+import os
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+
+JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
+env = gym_super_mario_bros.make(
+    "SuperMarioBros-v0", apply_api_compatibility=True, render_mode="human"
+)
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 env = GrayScaleObservation(env, keep_dim=True)
 env = DummyVecEnv([lambda: env])
-env = VecFrameStack(env, 4, channels_order='last')
+env = VecFrameStack(env, 4, channels_order="last")
 # check_env(env)
 
+
 # done = True
+class TrainAndLogg(BaseCallback):
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndLogg, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            self.model.save(
+                os.path.join(self.save_path, f"rl_model_{self.n_calls}.zip")
+            )
+        return True
+
+
+CHECK_DIR = "./train"
+LOG_DIR = "./logs"
+
 # for step in range(100000):
-#     if done: 
+#     if done:
 #         env.reset()
 #     state, reward, done, info = env.step(env.action_space.sample())
 #     env.render()
 # env.close()
 
-# done = True
-# state = env.reset()
-
+state = env.reset()
+# print(state.shape)
+state, reward, done, info = env.step([5])
 # plt.imshow(state[0])
+# plt.figure(figsize=(20, 16))
+# for idx in range(state.shape[3]):
+#     plt.subplot(1, 4, idx + 1)
+#     plt.imshow(state[0][:, :, idx])
+# plt.show()
 
 
-# for step in range(100000):
-#     action = env.action_space.sample()
-#     obs, reward, terminated, truncated, info = env.step(action)
-#     done = terminated or truncated
-#     if done:
-#         state = env.reset()
-# env.close()
+callback = TrainAndLogg(check_freq=10000, save_path=CHECK_DIR)
 
-
-
-
-# PREPROCESS ENVIRONMENT
-# Import gray scaling wrapper (less information for agent)
-# from gym.wrappers import GrayScaleObservation
-#Import vectorisation wrappers (VecFrameStack deals with multiple frame capture - allows agent to see direction enemies move)
-# from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-# Import matplotlib to show the impact of frame stacking
-# from matplotlib import pyplot as plt
-
-# Import OS for file path management
-# import os
-# Import PPO for algos
-# from stable_baselines3 import PPO
-# Import base callback for saving models
-# from stable_baselines3.common.callbacks import BaseCall
-
-
-# # 1. Create base environment
-# env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode='human')
-# #env = gym_super_mario_bros.make('SuperMarioBros-v0')
-# # 2. Simplify controls
-# env = JoypadSpace(env, SIMPLE_MOVEMENT)
-# # 3. Greyscale
-# env = GrayScaleObservation(env, keep_dim=True)
-# # 4. Wrap inside dummy environment
-# env = DummyVecEnv([lambda: env])
-# # 5. Stack the frames
-# env = VecFrameStack(env, 4, channels_order='last')
-
-
-# done = True
-# for step in range(100000):
-#     action = env.action_space.sample()
-#     # Do random actions
-#     state, reward, terminated, truncated, info = env.step(action)
-#     # Show game on screen
-#     env.render()
-#     if done:
-#         state = env.reset()
-# # Close the game
-# env.close()
-
-# # AI model
-# model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, n_steps=512)
-
-# #train ai model
-# model.learn(total_timesteps=1000000)
+model = PPO(
+    "CnnPolicy",
+    env,
+    verbose=1,
+    tensorboard_log=LOG_DIR,
+    learning_rate=0.000001,
+    n_steps=512,
+)
+model.learn(total_timesteps=100000, callback=callback)
