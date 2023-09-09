8d0d87c4d2001297159672ef49ff0618e2830acc
Crystal <crystalhellocrystal@gmail.com>
2023-09-09 11:23:13 +0800

    new mario RL tutorial PPO

--- agent/agent/app.py
+++ agent/agent/app.py
@@ -1,84 +1,108 @@
-# Install pytorch
-# poetry add torch torchvision torchaudio
-# install stable baselines for RL algorithm
-# poetry add stable-baselines3[extra]
-# pip3 install stable-baselines --use-deprecated=backtrack-on-build-failures
-# poetry add  stable-baselines3[extra]
+# pip install gym==0.21.0
+# pip install gym-retro
+# pip install stable-baselines3==1.6.0
 
-# https://stackoverflow.com/questions/73195438/openai-gyms-env-step-what-are-the-values
-
-# new
+import gym
+import numpy as np
+from RandomAgent import TimeLimitWrapper
 import gym_super_mario_bros
-from nes_py.wrappers import JoypadSpace
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-from gym.wrappers import frame_stack, GrayScaleObservation
-from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-from matplotlib import pyplot as plt
 
-# rl model
-import os
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import BaseCallback
-
-JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
-env = gym_super_mario_bros.make(
-    "SuperMarioBros-v0", apply_api_compatibility=True, render_mode="human"
-)
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
-env = GrayScaleObservation(env, keep_dim=True)
-env = DummyVecEnv([lambda: env])
-env = VecFrameStack(env, 4, channels_order="last")
-# check_env(env)
+from stable_baselines3.common.vec_env import VecMonitor
+from stable_baselines3.common.atari_wrappers import MaxAndSkipEnv
+import os
 
 
-# done = True
-class TrainAndLogg(BaseCallback):
-    def __init__(self, check_freq, save_path, verbose=1):
-        super(TrainAndLogg, self).__init__(verbose)
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq:
+    :param log_dir: Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: Verbosity level.
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
         self.check_freq = check_freq
-        self.save_path = save_path
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
 
-    def _init_callback(self):
+    def _init_callback(self) -> None:
+        # Create folder if needed
         if self.save_path is not None:
             os.makedirs(self.save_path, exist_ok=True)
 
-    def _on_step(self):
+    def _on_step(self) -> bool:
         if self.n_calls % self.check_freq == 0:
-            self.model.save(
-                os.path.join(self.save_path, f"rl_model_{self.n_calls}.zip")
-            )
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print(f"Saving new best model to {self.save_path}")
+                  self.model.save(self.save_path)
+
         return True
 
-
-CHECK_DIR = "./train"
-LOG_DIR = "./logs"
-
-# for step in range(100000):
-#     if done:
-#         env.reset()
-#     state, reward, done, info = env.step(env.action_space.sample())
-#     env.render()
-# env.close()
-
-state = env.reset()
-# print(state.shape)
-state, reward, done, info = env.step([5])
-# plt.imshow(state[0])
-# plt.figure(figsize=(20, 16))
-# for idx in range(state.shape[3]):
-#     plt.subplot(1, 4, idx + 1)
-#     plt.imshow(state[0][:, :, idx])
-# plt.show()
+# Create log dir
+log_dir = "tmp/"
+os.makedirs(log_dir, exist_ok=True)
 
 
-callback = TrainAndLogg(check_freq=10000, save_path=CHECK_DIR)
+def make_env(env_id, rank, seed=0):
+    """
+    Utility function for multiprocessed env.
 
-model = PPO(
-    "CnnPolicy",
-    env,
-    verbose=1,
-    tensorboard_log=LOG_DIR,
-    learning_rate=0.000001,
-    n_steps=512,
-)
-model.learn(total_timesteps=100000, callback=callback)
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environments you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    def _init():
+        #env = gym.make(env_id)
+        #env = retro.make(game=env_id)
+        env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode='human')
+        env = TimeLimitWrapper(env, max_steps=2000)
+        env = MaxAndSkipEnv(env, 4)
+        env.seed(seed + rank)
+        return env
+    set_random_seed(seed)
+    return _init
+
+if __name__ == '__main__':
+    env_id = "SuperMarioBros-Nes"
+    num_cpu = 4  # Number of processes to use
+    # Create the vectorized environment
+    env = VecMonitor(SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)]),"tmp/TestMonitor")
+    
+    # Stable Baselines provides you with make_vec_env() helper
+    # which does exactly the previous steps for you.
+    # You can choose between `DummyVecEnv` (usually faster) and `SubprocVecEnv`
+    # env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
+
+    model = PPO('CnnPolicy', env, verbose=1, tensorboard_log="./board/", learning_rate=0.00003)
+    #model = PPO.load("tmp/best_model", env=env)
+    print("------------- Start Learning -------------")
+    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
+    model.learn(total_timesteps=5000000, callback=callback, tb_log_name="PPO-00003")
+    model.save(env_id)
+    print("------------- Done Learning -------------")
